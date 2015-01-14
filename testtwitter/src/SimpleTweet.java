6016a6ab1fb552556a6db6a18d993da6db373adf
kouji suganuma <sug.for.github@gmail.com>
2015-01-14 21:56:44 +0900

    [追加]水分レベルからコメント取得機能追加

--- testtwitter/src/SimpleTweet.java
+++ testtwitter/src/SimpleTweet.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -9,18 +5,45 @@ import twitter4j.TwitterFactory;
 public class SimpleTweet {
 
 	public static void main(String[] args) {
+		int waterLevel = get_waterLevel();
 
-		System.out.println("What's happening???");
+		String comment = getComment(waterLevel);
+		//System.out.println("What's happening???");
 
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String tweetString = br.readLine();
+			//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			//String tweetString = br.readLine();
+			String tweetString = comment;
 
 			Twitter twitter = new TwitterFactory().getInstance();
 			twitter.updateStatus(tweetString);
 
-		} catch (IOException e) {
 		} catch (TwitterException e) {
 		}
 	}
+
+	private static int get_waterLevel() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+
+	private static String getComment(int waterLevel) {
+		// 水分レベルに応じたコメントを返す
+		String [] commentDB = new String [] {"もういらんて、水","腹八分目～","水を与えたまえ","水をください","水をくださいませんか","ちょっ、まじ水くれ"};
+
+		if(waterLevel >= 90){
+			return commentDB[0];
+		}else if((waterLevel >= 70) && (waterLevel < 90)){
+			return commentDB[1];
+		}else if((waterLevel >= 30) && (waterLevel < 70)){
+			return commentDB[2];
+		}else if((waterLevel >= 10) && (waterLevel < 30)){
+			return commentDB[3];
+		}else if((waterLevel >= 1) && (waterLevel < 10)){
+			return commentDB[4];
+		}else{
+			return commentDB[5];
+		}
+	}
 }
+
