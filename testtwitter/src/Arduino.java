b0950713ccdf0d61fb966565780927290ebe3177
tsuyoshi-h <higashiguchi.tsuyoshi.hi0@is.naist.jp>
2015-01-15 23:54:35 +0900

    <add Arduino.java&complete>
    Signed-off-by: tsuyoshi-h <higashiguchi.tsuyoshi.hi0@is.naist.jp>

--- testtwitter/src/Arduino.java
+++ testtwitter/src/Arduino.java
@@ -2,70 +2,55 @@
 import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
 
-import java.io.InputStream;
-	 
-	 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 	public class Arduino {
-		
+
 	 public static void main( String arg[] )
 	 {
-	   
+
 	  try
 	  {
-	 
+
 	   //=====================================================================================
 	   //シリアルポートを確保する
-	 
-	 
+
+
 	   //使用するCOMポートを取得
 	   CommPortIdentifier comID = CommPortIdentifier.getPortIdentifier( "COM4" );
-	 
+
 	   //COMポートを開きます
 	   CommPort commPort = comID.open("hoge",2000);
-	 
+
 	   //シリアルポートのインスタンスを生成…
 	   SerialPort port = (SerialPort)commPort;
-	 
-	 
-	 
+
+
+
 	   //=====================================================================================
 	   //シリアルポートの設定
-	 
-	 
+
+
 	   //ボーレート、データビット数、ストップビット数、パリティを設定
-	   port.setSerialPortParams( 115200,SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE );
-	 
+	   port.setSerialPortParams( 9600,SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE );
+
 	   //フロー制御はしない
 	   port.setFlowControlMode( SerialPort.FLOWCONTROL_NONE );
-	 
-	 
-	 
-	   InputStream is = port.getInputStream();
-	    
+
+
+
+	   BufferedReader is = new BufferedReader(new InputStreamReader(port.getInputStream()));
+
 	   int c; // arduinoからの出力読み込み用
-	   boolean update_flag = false; // DB updateフラグ
-	   boolean flag = true; // 無限ループ用フラグ
 	   System.out.println("start reading");
-	   int count=0; // 音声ファイル用カウンタ
 	   while(true){
-                  c = is.read();
-	      if (c != -1) {
-	    		  System.out.println(count+ "->" + c); // デバッグ用
-	    		  // arduinoでは1を出力するはずが、なぜか48 or 49になってる
-		    		  if (c > 48) { // ティッシュ抜き取り
-	     			  update_flag = true;
-		    		  } else {           
-	     			  update_flag = false;
-	    		  }
-	    		  if (update_flag) {
-	    			  qcount++;
-                                      update_flag = false;
-	                  }
-	      }
-	      if (flag == false) {
+                  String line = is.readLine();
+                  System.out.println(line); // デバッグ用
+
 	    	  break;
 	      }
-	   }
                is.close();
                port.close();
                System.out.println("finished reading");
@@ -74,6 +59,5 @@ import java.io.InputStream;
 	  {
 	   System.out.println( "Error発生:" + e );
 	  }
-	 
 	 }
 	}
